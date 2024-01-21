09773f2ef9417232b3aa682a3810d3efc0bf7a40
zlAxel <axel.macias17@hotmail.com>
2024-01-21 00:16:08 -0600

    Se suben layouts (titulos)

---
+++ src/app/nosotros/layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: 'Nosotros',
+    description: "Sitio web dedicado a la venta de guitarras",
+}
+
+export default function NosotrosLayout({ children }) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
